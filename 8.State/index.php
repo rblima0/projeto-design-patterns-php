90db6f1270439479a2f2ed5d8f6ff89d00948095
Rodrigo Banci <rblima0@gmail.com>
2016-12-07 23:40:16 -0200

    State - em andamento

---
+++ 8.State/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>State</title>
+</head>
+<body>
+	<h2>State No PHP</h2>
+	<p>Começando as mudanças</p>
+	
+</body>
+</html>
